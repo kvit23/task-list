741f042ffa2975290bd6c60e33fac6cab0fdc0eb
kvit23 <elsagheer9@gmail.com>
2024-01-22 03:12:43 +0200

    DONE

--- routes/web.php
+++ routes/web.php
@@ -75,6 +75,11 @@ Route::delete('/tasks/{task}', function(Task $task){
     return redirect()->route('tasks.index')->with('success', 'Task Deleted!');
 })->name('tasks.destroy');
 
+Route::put('tasks/{task}/toggle-complete', function(Task $task){
+    $task->toggleComplete();
+    return redirect()->back()->with('success', 'Task updated!');
+})->name('tasks.toggle-complete');
+
 // Route::get('/{id}', function($id){
 //     return 'One single task';
 // })->name('tasks.show');
